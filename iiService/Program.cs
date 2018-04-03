1f10fb1f456a9956e8aa6d4ab2df195453e147fe
PaulsonLiu <paulsonliu@163.com>
2018-04-04 00:36:04 +0800

    iiservice改名字

--- iiService/Program.cs
+++ iiService/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace iiService
+namespace iiService.Api
 {
     public class Program
     {
